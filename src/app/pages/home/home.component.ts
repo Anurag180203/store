658d21ccbeed3b9749ce52260e5d3c0d190537f9
Anurag180203 <anurag180203@gmail.com>
2023-04-05 17:14:36 +0530

    added store api and linked limit as well as sort

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,6 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
 import { Product } from 'src/app/models/product.model';
 import { CartService } from 'src/app/services/cart.service';
+import { StoreService } from 'src/app/services/store.service';
 
 const ROWS_HEIGHT :{[id:number]:number}= { 1:400, 3:335 , 4:350}
 
@@ -10,13 +12,33 @@ const ROWS_HEIGHT :{[id:number]:number}= { 1:400, 3:335 , 4:350}
   styles: [
   ]
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit,OnDestroy {
   cols=3;
   rowHeight=ROWS_HEIGHT[this.cols];
   category:string | undefined;
-  constructor(private cartService: CartService) { }
+  products: Array<Product> | undefined;
+  sort='desc';
+  count='12';
+  productsSubscription:Subscription | undefined;
+
+
+  constructor(private cartService: CartService, private storeService: StoreService) { }
 
   ngOnInit(): void {
+    this.getProducts();
+  }
+
+  ngOnDestroy(): void {
+      if(this.productsSubscription){
+        this.productsSubscription.unsubscribe();
+      }
+  }
+
+  getProducts():void {
+    this.productsSubscription = this.storeService.getAllProducts(this.count,this.sort)
+    .subscribe((_products) => {
+      this.products = _products;
+    });
   }
 
   onColumnsCountChange(colsNum: number):void {
@@ -36,5 +58,15 @@ export class HomeComponent implements OnInit {
       quantity: 1,
       id: product.id
     });
+  }
+
+  onItemsCountChange(newcount:number):void{
+    this.count=newcount.toString();
+    this.getProducts();
+  }
+
+  onSortChange(newSort:string):void{
+    this.sort=newSort;
+    this.getProducts();
   }
 }
