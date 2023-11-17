70873de0b6c66aa648b65cbac309a52d9c69df6c
Anurag180203 <anurag180203@gmail.com>
2023-11-17 11:17:42 +0530

    added a form at the end of cart page

--- src/app/services/store.service.ts
+++ src/app/services/store.service.ts
@@ -4,6 +4,7 @@ import { Observable } from 'rxjs';
 import { Product } from '../models/product.model';
 
 const STORE_BASE_URL = 'https://fakestoreapi.com';
+// const STORE_BASE_URL = 'http://localhost:4000';
 
 @Injectable({
   providedIn: 'root',
@@ -22,6 +23,15 @@ export class StoreService {
       }?sort=${sort}&limit=${limit}`
     );
   }
+  // getAllProducts(
+  //   limit = '12',
+  //   sort = 'desc',
+  //   category?: string
+  // ): Observable<Array<Product>> {
+  //   return this.httpClient.get<Array<Product>>(
+  //     `${STORE_BASE_URL}`
+  //   );
+  // }
 
   getAllCategories(): Observable<Array<string>> {
     return this.httpClient.get<Array<string>>(
